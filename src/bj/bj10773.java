4f2b9f0ff133485255c8f8cea1de643a3537e7e5
hobit22 <hobit22@naver.com>
2022-05-03 10:39:21 +0900

    제로 백준 10773

---
+++ src/bj/bj10773.java
@@ -0,0 +1,25 @@
+package bj;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class bj10773 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+        while(tc-- > 0){
+            int num = sc.nextInt();
+            if(num != 0){
+                stack.push(num);
+            }else{
+                stack.pop();
+            }
+        }
+        while(!stack.empty()) {
+            answer += stack.pop();
+        }
+        System.out.println(answer);
+    }
+}
