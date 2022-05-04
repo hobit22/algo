d7683f81ed1d71a6b47ff14b55e50d1e5a26c296
hobit22 <hobit22@naver.com>
2022-05-04 11:55:41 +0900

    스택 수열 백준 1874

---
+++ src/bj/bj1874.java
@@ -0,0 +1,33 @@
+package bj;
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class bj1874 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = in.nextInt();
+        int check = 0;
+        Stack<Integer> stack = new Stack<>();
+        while(n-- > 0){
+            int target = in.nextInt();
+            if(target > check){
+                for(int i = check + 1 ; i <= target ; i++){
+                    stack.push(i);
+                    sb.append("+").append("\n");
+                }
+                check = target;
+            }else if(stack.peek() != target){
+                System.out.println("NO");
+                return;
+            }
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
