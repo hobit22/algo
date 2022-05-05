bb5d8d90efd27555b7d2da8b8ae745e2a7d67462
hobit22 <hobit22@naver.com>
2022-05-05 15:20:15 +0900

    괄호 코맨트 추가 백준 9012

--- src/bj/bj9012.java
+++ src/bj/bj9012.java
@@ -1,33 +1,41 @@
 package bj;
 
-import java.util.*;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class bj9012 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < tc ; i++){
+        // Test Case 만큼 for 문 돌기
+        for (int i = 0; i < tc; i++) {
             String ps = br.readLine();
+
+            // TC 마다 stack 새로 생성
             Stack<String> stack = new Stack<>();
             String answer = "";
-            for(int j = 0 ; j < ps.length(); j++){
-                String check = ps.substring(j,j+1);
-                if(check.equals("(")){
+            for (int j = 0; j < ps.length(); j++) {
+
+                //pc 에서 문자1개씩 잘라서 저장
+                String check = ps.substring(j, j + 1);
+                if (check.equals("(")) {
                     stack.push(check);
-                }else if(check.equals(")")){
-                    if(stack.isEmpty()){
+                } else if (check.equals(")")) {
+
+                    // ) 인데 스택이 비어있는 경우 break
+                    if (stack.isEmpty()) {
                         answer = "NO";
                         break;
                     }
                     stack.pop();
                 }
             }
-            if(stack.isEmpty() && answer.equals("")){
+
+            if (stack.isEmpty() && answer.equals("")) {
                 System.out.println("YES");
-            }else {
+            } else {
                 System.out.println("NO");
             }
         }
