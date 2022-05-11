e45c06ee88f66e8312d19347f353d6d05f0e19f3
hobit22 <hobit22@naver.com>
2022-05-11 23:23:29 +0900

    영화감독 숌 백준 1436

---
+++ src/bj/bj1436.java
@@ -0,0 +1,24 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj1436 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int start = 666;
+        while(true){
+            String startString = Integer.toString(start);
+
+            if(startString.contains("666")){
+                n--;
+                if(n==0) break;
+            }
+            start++;
+        }
+        System.out.println(start);
+
+    }
+}
