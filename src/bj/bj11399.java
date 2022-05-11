e5644ada0a70de26d0a7aa9e944987a0abfff00e
hobit22 <hobit22@naver.com>
2022-05-11 23:17:40 +0900

    ATM 백준 11399

---
+++ src/bj/bj11399.java
@@ -0,0 +1,24 @@
+package bj;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class bj11399 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        ArrayList<Integer> timeList = new ArrayList<>();
+        for (int i = 0; i < tc; i++) {
+            timeList.add(sc.nextInt());
+        }
+        Collections.sort(timeList);
+
+        int total = 0;
+        for(int i = 0 ; i < timeList.size(); i++) {
+            total += timeList.get(i) *(tc - i);
+        }
+        System.out.println(total);
+
+    }
+}
