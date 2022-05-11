e6931bc28a2b6f5c5dc642b4911f86b16e095661
hobit22 <hobit22@naver.com>
2022-05-11 23:49:07 +0900

    다리 놓기 백준 1010

---
+++ src/bj/bj1010.java
@@ -0,0 +1,26 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int r = sc.nextInt();
+            int n = sc.nextInt();
+
+            //nCr 구하기
+
+            int answer = 1;
+            if(r!=n){
+                for(int i = 1; i<=r;i++){
+                    answer = answer *(n-i+1);
+                    answer = answer/i;
+                }
+            }
+
+            System.out.println(answer);
+        }
+    }
+}
