f1f4c90b3d6fdc5261a3b7ca790d5c207f3c3021
hobit22 <hobit22@naver.com>
2022-05-11 23:01:33 +0900

    동전0 백준 11047

---
+++ src/bj/bj11047.java
@@ -0,0 +1,26 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj11047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        int k = sc.nextInt();
+        int[] coins = new int[tc];
+        for (int i = 0; i < tc; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        int total = 0;
+        for (int j = tc - 1; j >= 0; j--) {
+            if (k == 0) break;
+            if (k / coins[j] > 0) {
+                int division = k / coins[j];
+                total += division;
+                k = k - division * coins[j];
+            }
+        }
+        System.out.println(total);
+    }
+}
