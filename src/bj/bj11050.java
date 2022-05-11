f1e011fe603cf4d5b12965e0ffefee0e05f653d2
hobit22 <hobit22@naver.com>
2022-05-11 23:42:05 +0900

    이항 계수 1 백준 11050

---
+++ src/bj/bj11050.java
@@ -0,0 +1,21 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj11050 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int answer = 1;
+
+        for (int i = 0; i <k ; i++) {
+            answer = answer * (n - i);
+        }
+        for (int i = 1; i <= k; i++) {
+            answer = answer / i;
+        }
+        System.out.println(answer);
+    }
+}
