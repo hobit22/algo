8e5cb97c1a8db56cae67870cd3558a3475d28165
hobit22 <hobit22@naver.com>
2022-05-11 23:36:05 +0900

    최대공약수와 최소공배수 백준 2609

---
+++ src/bj/bj2609.java
@@ -0,0 +1,26 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj2609 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int x = gcd(n, m);
+        int y = n * m / x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public static int gcd(int a, int b) {
+        int x = a % b;
+        while (x > 0) {
+            a = b;
+            b = x;
+            x = a % b;
+        }
+        return b;
+    }
+}
