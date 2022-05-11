4e66b8a57f954560bed5e5a8c1c095a3b58078f8
hobit22 <hobit22@naver.com>
2022-05-11 23:32:31 +0900

    분해합 백준 2231

---
+++ src/bj/bj2231.java
@@ -0,0 +1,27 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj2231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int answer = 1;
+        while(true){
+            int sum = 0;
+            int tmp = answer;
+            while(tmp != 0){
+                sum += tmp % 10;
+                tmp = tmp / 10;
+            }
+            if(sum + answer == n) break;
+            answer++;
+            if(answer >= n){
+                System.out.println(0);
+                return;
+            }
+        }
+        System.out.println(answer);
+    }
+}
