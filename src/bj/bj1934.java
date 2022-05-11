4ff50379f96326d9e0eee99a61f602babf99aeb9
hobit22 <hobit22@naver.com>
2022-05-11 14:56:33 +0900

    최소공배수 백준 1934

---
+++ src/bj/bj1934.java
@@ -0,0 +1,27 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj1934 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc--> 0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int max_divisor = gcd(a,b);
+            int min_multiple = a * b / max_divisor;
+            System.out.println(min_multiple);
+        }
+    }
+
+    public static int gcd(int a, int b){
+        int x = a % b;
+        while(x > 0){
+            a = b;
+            b = x;
+            x = a%b;
+        }
+        return b;
+    }
+}
