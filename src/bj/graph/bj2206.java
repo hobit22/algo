e370d0945db5da45c7647acd57e5b26eff266f96
hobit22 <hobit22@naver.com>
2022-06-11 18:28:58 +0900

    벽 부수고 이동하기 bj 2206 bfs

--- src/bj/graph/bj2206.java
+++ src/bj/graph/bj2206.java
@@ -16,11 +16,9 @@ import java.util.StringTokenizer;
 public class bj2206 {
     static int n, m;
     static int[][] map;
-    static int[][] score;
-    static boolean[][] visited;
+    static int[][] visited;
     static int[] dx = {-1, 1, 0, 0,};
     static int[] dy = {0, 0, -1, 1,};
-    static int result = Integer.MAX_VALUE;
 
     //Todo 시간초과 해결해야함
     public static void main(String[] args) throws IOException {
@@ -31,43 +29,32 @@ public class bj2206 {
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n+1][m+1];
+        visited = new int[n+1][m+1];
 
         // map 입력받기
         for (int i = 0; i < n; i++) {
             String s = bf.readLine();
             for (int j = 0; j < m; j++) {
                 map[i+1][j+1] = s.charAt(j) - '0';
+                visited[i+1][j+1] = Integer.MAX_VALUE;
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (map[i][j] == 1) {
-                    map[i][j] = 0;
-                    bfs();
-                    map[i][j] = 1;
-                }else{
-                    bfs();
-                }
-            }
-        }
-        System.out.println(result == Integer.MAX_VALUE ? -1 : result);
+        int ans = bfs();
+
+        System.out.println(ans);
 
     }
 
-    private static void bfs() {
+    private static int bfs() {
         Queue<Point> q = new LinkedList<>();
-        q.add(new Point(1,1));
+        q.add(new Point(1,1, 1, 0));
+        visited[1][1] = 0;
 
-        score = new int[n+1][m+1];
-        score[1][1] = 1;
-
-        visited = new boolean[n+1][m+1];
-        visited[1][1] = true;
         while (!q.isEmpty()) {
             Point now = q.remove();
 
-            if(now.x == n && now.y == m) break;
+            if(now.x == n && now.y == m) return now.distance;
 
             for (int i = 0; i < 4; i++) {
                 int nextX = now.x + dx[i];
@@ -75,29 +62,31 @@ public class bj2206 {
 
                 // 범위 체크
                 if(nextX < 1 || nextX > n || nextY < 1 || nextY > m) continue;
-                // 벽체크
-                if(map[nextX][nextY] == 1) continue;
 
-                if(visited[nextX][nextY]) continue;
+                if(visited[nextX][nextY] <= now.drill) continue;
 
-                q.add(new Point(nextX, nextY));
-                visited[nextX][nextY] = true;
-                score[nextX][nextY] = score[now.x][now.y] + 1;
+                if (map[nextX][nextY] == 0) {
+                    q.add(new Point(nextX, nextY, now.distance + 1, now.drill));
+                    visited[nextX][nextY] = now.drill;
+                } else {
+                    if (now.drill == 0) {
+                        q.add(new Point(nextX, nextY, now.distance + 1, now.drill + 1));
+                        visited[nextX][nextY] = now.drill + 1;
+                    }
+                }
             }
         }
-
-        if(score[n][m] != 0){
-            result = Math.min(result, score[n][m]);
-        }
+        return -1;
     }
 
     static class Point{
-        int x;
-        int y;
+        int x, y, distance, drill;
 
-        public Point(int x, int y) {
+        public Point(int x, int y, int distance, int drill) {
             this.x = x;
             this.y = y;
+            this.distance = distance;
+            this.drill = drill;
         }
     }
 
