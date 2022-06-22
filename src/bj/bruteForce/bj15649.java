e36e9c68374a2bde469fd219aa16d4fdb28fc655
hobit22 <hobit22@naver.com>
2022-06-23 07:27:51 +0900

    N과 M(1) bj 15649 백트래킹

---
+++ src/bj/bruteForce/bj15649.java
@@ -0,0 +1,49 @@
+package bj.bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * N과 M(1)
+ * 백트래킹
+ */
+public class bj15649 {
+    static int n, m;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visited = new boolean[n];
+
+        combination(0);
+        System.out.println(sb);
+    }
+
+    private static void combination(int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                combination(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
