e63e500823359676c9f8b1a228ef47ac4ebb91d7
hobit22 <hobit22@naver.com>
2022-07-05 22:14:02 +0900

    로봇 청소기 bf 14503 // todo 작동하는 알고리즘 코딩

---
+++ src/bj/graph/bj14503.java
@@ -0,0 +1,48 @@
+package bj.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj14503 {
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+    static Point now;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        st = new StringTokenizer(br.readLine());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        now = new Point(r, c, d);
+
+
+
+    }
+
+    private static class Point{
+        int x;
+        int y;
+        int dir;
+
+        public Point(int x, int y, int dir) {
+            this.x = x;
+            this.y = y;
+            this.dir = dir;
+        }
+    }
+}
