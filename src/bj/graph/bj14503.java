e1ffdc851a5117f025185e94efe69c32741cf9de
hobit22 <hobit22@naver.com>
2022-07-06 23:10:55 +0900

    로봇 청소기 bf 14503

--- src/bj/graph/bj14503.java
+++ src/bj/graph/bj14503.java
@@ -8,10 +8,10 @@ import java.util.StringTokenizer;
 public class bj14503 {
     static int n, m;
     static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {-1, 0, 1, 0};
-    static Point now;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    static int answer;
+    static boolean flag;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,7 +20,6 @@ public class bj14503 {
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        visited = new boolean[n][m];
 
         st = new StringTokenizer(br.readLine());
 
@@ -28,21 +27,57 @@ public class bj14503 {
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
 
-        now = new Point(r, c, d);
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
+        clean(r, c, d);
 
-
+        System.out.println(answer);
     }
 
-    private static class Point{
-        int x;
-        int y;
-        int dir;
+    private static void clean(int r, int c, int d) {
+        if (map[r][c] == 0) {
+            map[r][c] = 2;
+            answer++;
+        }
 
-        public Point(int x, int y, int dir) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
+        flag = false;
+        int ori_d = d;
+        // 4 방향 체크
+        for (int i = 0; i < 4; i++) {
+            // 왼쪽부터 체크
+            int nd = (d + 3) % 4;
+            int nr = r + dr[nd];
+            int nc = c + dc[nd];
+
+            // 범위체크
+            if (nr > 0 && nc > 0 && nr < n && nc < m) {
+                // 0인지 확인
+                if (map[nr][nc] == 0) {
+                    clean(nr, nc, nd);
+                    flag = true;
+                    break;
+                }
+            }
+            d = (d + 3) % 4;
+        }
+
+        // 4방향 갈 곳이 없을 때
+        if (!flag) {
+            // 후진
+            int nd = (ori_d + 2) % 4;
+            int nr = r + dr[nd];
+            int nc = c + dc[nd];
+
+            if (nr > 0 && nc > 0 && nr < n && nc < m) {
+                if (map[nr][nc] != 1) {
+                    clean(nr, nc, ori_d);
+                }
+            }
         }
     }
 }
