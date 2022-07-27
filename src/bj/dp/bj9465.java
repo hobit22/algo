065c6efce24e481eb8b612e83c95b25c118871da
hobit22 <hobit22@naver.com>
2022-07-27 22:37:16 +0900

    백준 9465 스티커 dp

---
+++ src/bj/dp/bj9465.java
@@ -0,0 +1,40 @@
+package bj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj9465 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int tc = Integer.parseInt(st.nextToken());
+
+        while (tc-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            int[][] arr = new int[2][n + 1];
+            int[][] dp = new int[2][n + 1];
+
+            for (int i = 0; i < 2; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j < n + 1; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[0][1] = arr[0][1];
+            dp[1][1] = arr[1][1];
+
+            for (int i = 2; i < n + 1; i++) {
+                dp[0][i] = Math.max(dp[1][i - 1], dp[1][i - 2]) + arr[0][i];
+                dp[1][i] = Math.max(dp[0][i - 1], dp[0][i - 2]) + arr[1][i];
+            }
+            System.out.println(Math.max(dp[0][n], dp[1][n]));
+
+        }
+    }
+}
