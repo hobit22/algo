cd5e46d0a02278100cdd1e32789bd007e08307c5
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2023-03-14 00:32:44 +0900

    2023 KAKAO BLIND RECRUITMENT 표 병합
    참고 : https://velog.io/@gale4739/2023-KAKAO-BLIND-RECRUITMENT-%ED%91%9C-%EB%B3%91%ED%95%A9

--- src/programmers/kakao/blind2023/표_병합.java
+++ src/programmers/kakao/blind2023/표_병합.java
@@ -7,26 +7,66 @@ import java.util.List;
 public class 표_병합 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String[] answerA = solution.solution(new String[]{"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"});
-        String[] answerB = solution.solution(new String[]{"UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"});
+        String[] answerA = solution.solution(new String[]{
+                "UPDATE 1 1 menu",
+                "UPDATE 1 2 category",
+                "UPDATE 2 1 bibimbap",
+                "UPDATE 2 2 korean",
+                "UPDATE 2 3 rice",
+                "UPDATE 3 1 ramyeon",
+                "UPDATE 3 2 korean",
+                "UPDATE 3 3 noodle",
+                "UPDATE 3 4 instant",
+                "UPDATE 4 1 pasta",
+                "UPDATE 4 2 italian",
+                "UPDATE 4 3 noodle",
+
+                "MERGE 1 2 1 3",
+                "MERGE 1 3 1 4",
+
+                "UPDATE korean hansik",
+                "UPDATE 1 3 group",
+
+                "UNMERGE 1 4",
+
+                "PRINT 1 3", "PRINT 1 4"});
+        String[] answerB = solution.solution(new String[]{
+                "UPDATE 1 1 a",
+                "UPDATE 1 2 b",
+                "UPDATE 2 1 c",
+                "UPDATE 2 2 d",
+
+                "MERGE 1 1 1 2",
+                "MERGE 2 2 2 1",
+                "MERGE 2 1 1 1",
+
+                "PRINT 1 1",
+
+                "UNMERGE 2 2",
+
+                "PRINT 1 1"});
 
         System.out.println("answerA = " + Arrays.toString(answerA));
         System.out.println("answerB = " + Arrays.toString(answerB));
     }
 
     static class Solution {
-        Cell[][] cellTable = new Cell[51][51];
+        static String[][] map = new String[51][51];
+        static Pos[][] ref = new Pos[51][51];
 
         public String[] solution(String[] commands) {
             List<String> answer = new ArrayList<>();
 
             for (int x = 1; x < 51; x++) {
                 for (int y = 1; y < 51; y++) {
-                    cellTable[x][y] = new Cell();
+                    ref[x][y] = new Pos(x, y);
                 }
             }
 
             for (String command : commands) {
+                if (command.contains("group")) {
+                    System.out.println("command = " + command);
+                }
                 String[] s = command.split(" ");
 
                 switch (s[0]) {
@@ -50,105 +90,109 @@ public class 표_병합 {
         }
 
         private void unmergeCell(String[] s) {
-            int commandX = Integer.parseInt(s[1]);
-            int commandY = Integer.parseInt(s[2]);
+            int x = Integer.parseInt(s[1]);
+            int y = Integer.parseInt(s[2]);
 
-            Cell targetCell = cellTable[commandX][commandY];
-            Cell parentCell = findParentCell(targetCell);
+            int newx = ref[x][y].x;
+            int newy = ref[x][y].y;
 
-            targetCell.value = parentCell.value;
-            targetCell.parentCell = null;
+            String str = map[newx][newy];
 
-            for (int x = 1; x < 51; x++) {
-                for (int y = 1; y < 51; y++) {
-                    Cell searchCell = cellTable[x][y];
-                    if (searchCell.parentCell == parentCell) {
-                        searchCell.value = "";
-                        searchCell.parentCell = null;
+            for (int i = 1; i < 51; i++) {
+                for (int j = 1; j < 51; j++) {
+
+                    if (ref[i][j].x == newx && ref[i][j].y == newy) {
+                        ref[i][j] = new Pos(i, j);
+                        map[i][j] = null;
                     }
+
                 }
             }
+
+            map[x][y] = str;
 
         }
 
         private void mergeCell(String[] s) {
             int x1 = Integer.parseInt(s[1]);
             int y1 = Integer.parseInt(s[2]);
-            Cell cell1 = cellTable[x1][y1];
-            Cell parentCell1 = cell1.parentCell != null ? findParentCell(cell1) : null;
-
-            Cell tmp1 = parentCell1 == null ? cell1 : parentCell1;
 
             int x2 = Integer.parseInt(s[3]);
             int y2 = Integer.parseInt(s[4]);
-            Cell cell2 = cellTable[x2][y2];
-            Cell parentCell2 = cell2.parentCell != null ? findParentCell(cell2) : null;
 
-            Cell tmp2 = parentCell2 == null ? cell2 : parentCell2;
+            int newx1 = ref[x1][y1].x;
+            int newy1 = ref[x1][y1].y;
+            int newx2 = ref[x2][y2].x;
+            int newy2 = ref[x2][y2].y;
 
-            if (cell1 == cell2) {
-            } else if (tmp1.value != "") {
-                tmp2.value = "";
-                tmp2.parentCell = tmp1;
-            } else if (tmp2.value == "") {
-                tmp1.value = "";
-                tmp1.parentCell = tmp2;
-            }
-        }
-
-        private Cell findParentCell(Cell cell) {
-            if (cell.value != "") return cell;
-
-            return findParentCell(cell.parentCell);
-        }
-
-        private String printCell(String[] s) {
-            Cell cell = cellTable[Integer.parseInt(s[1])][Integer.parseInt(s[2])];
-
-            if (cell.parentCell != null) {
-                return findParentCell(cell).value;
-            } else if (cell.value != "") {
-                return cell.value;
-            } else {
-                return "EMPTY";
-            }
-        }
-
-        private void updateCell(String[] s) {
-            if (s.length == 3) {
-                changeValue(s[1], s[2]);
-            } else {
-                insertValue(Integer.parseInt(s[1]), Integer.parseInt(s[2]), s[3]);
-            }
-        }
-
-        private void insertValue(int x, int y, String value) {
-            //check
-            Cell cell = cellTable[x][y];
-            Cell parentCell = cell.parentCell != null ? findParentCell(cell) : null;
-
-            if (parentCell == null) {
-                cell.value = value;
-            } else {
-                parentCell.value = value;
-            }
-        }
-
-        private void changeValue(String searchStr, String replaceStr) {
-            for (int x = 1; x < 51; x++) {
-                for (int y = 1; y < 51; y++) {
-                    if (cellTable[x][y].value.equals(searchStr)) {
-                        cellTable[x][y].value = replaceStr;
+            for (int i = 1; i < 51; i++) {
+                for (int j = 1; j < 51; j++) {
+                    if (ref[i][j].x == newx2 && ref[i][j].y == newy2) {
+                        ref[i][j] = new Pos(newx1, newy1);
                     }
+                }
+            }
+
+            if (newx1 != newx2 || newy1 != newy2) {
+                // 두 셀 중 한 셀이 값을 가지고 있을 경우 병합된 셀은 그 값을 가짐
+                if (map[newx1][newy1] != null && map[newx2][newy2] == null) {
+                    map[newx2][newy2] = map[newx1][newy1];
+                } else if (map[newx1][newy1] == null && map[newx2][newy2] != null) {
+                    map[newx1][newy1] = map[newx2][newy2];
+                }
+
+                // 두 셀 모두 값을 가지고 있을 경우 병합된 셀은 r1,c1 위치의 셀 값 가짐
+                else if (map[newx1][newy1] != null && map[newx2][newy2] != null) {
+                    map[newx2][newy2] = map[newx1][newy1];
                 }
             }
 
         }
 
 
-        private class Cell {
-            String value = "";
-            Cell parentCell = null;
+        private String printCell(String[] s) {
+            int x = Integer.parseInt(s[1]);
+            int y = Integer.parseInt(s[2]);
+
+            int newx = ref[x][y].x;
+            int newy = ref[x][y].y;
+
+            String str = map[newx][newy];
+
+            if (str == null) return "EMPTY";
+            else return str;
+        }
+
+        private void updateCell(String[] s) {
+            if (s.length == 4) {
+                int x = Integer.parseInt(s[1]);
+                int y = Integer.parseInt(s[2]);
+                String str = s[3];
+
+                map[ref[x][y].x][ref[x][y].y] = str;
+            } else {
+                String str1 = s[1];
+                String str2 = s[2];
+
+                for (int i = 1; i < 51; i++) {
+                    for (int j = 1; j < 51; j++) {
+                        if (map[i][j] == null) continue;
+                        if (map[i][j].equals(str1)) {
+                            map[i][j] = str2;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static class Pos {
+            int x;
+            int y;
+
+            public Pos(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
         }
     }
 }
