940b0fc8db1e664f37988e7057b4273a0aaf7a40
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2023-03-15 22:44:24 +0900

    테스트 all pass, 효율성 2 pass 7 fail

---
+++ src/programmers/kakao/tech2022/행렬과_연산.java
@@ -0,0 +1,116 @@
+package programmers.kakao.tech2022;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class 행렬과_연산 {
+    public static void main(String[] args) {
+
+        /**
+         * rc	operations	result
+         * [[1, 2, 3], [4, 5, 6], [7, 8, 9]]	["Rotate", "ShiftRow"]	[[8, 9, 6], [4, 1, 2], [7, 5, 3]]
+         * [[8, 6, 3], [3, 3, 7], [8, 4, 9]]	["Rotate", "ShiftRow", "ShiftRow"]	[[8, 3, 3], [4, 9, 7], [3, 8, 6]]
+         * [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]	["ShiftRow", "Rotate", "ShiftRow", "Rotate"]	[[1, 6, 7 ,8], [5, 9, 10, 4], [2, 3, 12, 11]]
+         */
+
+        Solution solution = new Solution();
+
+//        int[][] solution1 = solution.solution(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, new String[]{"Rotate", "ShiftRow"});
+//        int[][] solution2 = solution.solution(new int[][]{{8, 6, 3}, {3, 3, 6}, {8, 4, 9}}, new String[]{"Rotate", "ShiftRow", "ShiftRow"});
+        int[][] solution3 = solution.solution(new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}, new String[]{"ShiftRow", "Rotate", "ShiftRow", "Rotate"});
+        int[][] solution4 = solution.solution(new int[][]{{1,2}, {3,4}}, new String[]{"ShiftRow", "Rotate", "ShiftRow", "Rotate"});
+
+//        System.out.println("solution1 = " + Arrays.deepToString(solution1));
+//        System.out.println("solution2 = " + Arrays.deepToString(solution2));
+        System.out.println("solution3 = " + Arrays.deepToString(solution3));
+        System.out.println("solution4 = " + Arrays.deepToString(solution4));
+    }
+
+    static class Solution {
+        static int[] dr = new int[]{0, 1, 0, -1};
+        static int[] dc = new int[]{1, 0, -1, 0};
+        int MAX_ROW = 0;
+        int MAX_COL = 0;
+
+        public int[][] solution(int[][] rc, String[] operations) {
+            MAX_ROW = rc.length;
+            MAX_COL = rc[0].length;
+
+            int[][] answer = {};
+            int[][] map = rc;
+
+            for (String operation : operations) {
+                System.out.println("operation = " + operation);
+                printMap(map);
+
+                System.out.println("===============================");
+                if (operation.equals("ShiftRow")) {
+                    map = shiftRow(map);
+                } else if (operation.equals("Rotate")) {
+                    map = rotate(map);
+                }
+                printMap(map);
+
+            }
+
+
+            return map;
+        }
+
+        private int[][] rotate(int[][] map) {
+            int[][] newMap = new int[map.length][map[0].length];
+
+            for (int i = 0; i < newMap.length; i++) {
+                for (int j = 0; j < newMap[i].length; j++) {
+                    newMap[i][j] = map[i][j];
+                }
+            }
+
+            int r = 0;
+            int c = 0;
+
+            for (int i = 0; i < 4; i++) {
+
+                int targetR = r + dr[i];
+                int targetC = c + dc[i];
+                while (targetR < map.length && targetC < map[0].length && targetR >= 0 && targetC >= 0) {
+                    newMap[targetR][targetC] = map[r][c];
+
+                    r = targetR;
+                    c = targetC;
+
+                    targetR = r + dr[i];
+                    targetC = c + dc[i];
+                }
+            }
+
+            return newMap;
+        }
+
+        private void printMap(int[][] rc) {
+            for (int[] ints : rc) {
+                for (int anInt : ints) {
+                    System.out.print(anInt + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        private int[][] shiftRow(int[][] map) {
+            // find last row
+            int lastRowIdx = map.length - 1;
+            int[] lastRow = map[lastRowIdx];
+
+            for (int i = lastRowIdx - 1; i >= 0; i--) {
+                int[] targetRow = map[i];
+                map[i + 1] = targetRow;
+            }
+
+            map[0] = lastRow;
+            return map;
+        }
+
+
+    }
+
+}
