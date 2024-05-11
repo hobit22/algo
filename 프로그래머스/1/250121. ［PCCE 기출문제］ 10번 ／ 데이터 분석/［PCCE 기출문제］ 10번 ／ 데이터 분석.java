91d11fb1276d3d76c9ba7e241681a847167b88ff
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2024-05-11 16:17:54 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.56 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        List<int[]> list = new ArrayList<>();
+        String[] s = {"code", "date", "maximum", "remain"};
+        int extIdx = 0, sortIdx = 0;
+
+        for (int i = 0; i < 4; i++) {
+            if (ext.equals(s[i])) extIdx = i;
+            if (sort_by.equals(s[i])) sortIdx = i;
+        }
+
+        final int si = sortIdx;
+
+        for (int[] datum : data) {
+            if (datum[extIdx] < val_ext) list.add(datum);
+        }
+
+        list.sort((o1, o2) -> o1[si] - o2[si]);
+
+        return list.toArray(new int[0][]);
+    }
+}
